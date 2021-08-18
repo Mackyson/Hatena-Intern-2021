5372d388bdeaec6045556a2d0d3f986a7007b5bd
intern <intern@HatenanoMacBook-Pro.local>
2021-08-18 16:06:41 +0900

    必須記法のテスト

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -8,8 +8,40 @@ import (
 )
 
 func Test_Render(t *testing.T) {
-	src := `foo https://google.com/ bar`
-	html, err := Render(context.Background(), src)
-	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+	tests := []struct {
+		src    string
+		expect string
+	}{
+		{
+			src: "# hoge\n## fuga",
+			expect: `<h1>hoge</h1>
+<h2>fuga</h2>
+`,
+		},
+		{
+			src: `* a
+* b
+1. 1
+1. 2`,
+			expect: `<ul>
+<li>a</li>
+<li>b</li>
+</ul>
+<ol>
+<li>1</li>
+<li>2</li>
+</ol>
+`,
+		},
+		{
+			src: `[google](https://google.com/)`,
+			expect: `<p><a href="https://google.com/">google</a></p>
+`,
+		},
+	}
+	for _, test := range tests {
+		html, err := Render(context.Background(), test.src)
+		assert.NoError(t, err)
+		assert.Equal(t, test.expect, html)
+	}
 }
